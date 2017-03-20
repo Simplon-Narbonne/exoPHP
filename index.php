e9ae2acdc8945580d6c5ecb2884bb731fb6ce8dc
Hermeline ROUSSELET <bibouille66@gmail.com>
2017-03-20 14:06:58 +0100

    solutions

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
-
+<?php include("includes/header.php"); ?>
 <!-- Exo 2 -->
     <p>
-      Nous sommes le : <br />
-      Il est :
+      Nous sommes le : <?php  setlocale(LC_TIME, 'FR','fr_FR.UTF8', 'fr.UTF8', 'fr_FR.UTF-8', 'fr.UTF-8');
+echo strftime('%d %B %Y', time()); ?>  <br />
+Il est : <?php echo date('H:i:s'); ?>
     </p>
 
 <!-- Exo 3 -->
@@ -14,4 +15,13 @@ $personnes = array(
   3 => array('prenom' => 'Marta', 'nom' => 'Blanca', 'telephone' => '003311111'),
   4 => array('prenom' => 'John', 'nom' => 'Doe', 'telephone' => '004411111')
 );
+foreach ($personnes as $numero) {
+  foreach ($numero as $qqun => $value){
+
+  }
+  if (in_array('Dain', $numero)){
+    echo "J'ai trouvé Mme Dain ";
+  }
+}
+include("includes/footer.php");
 ?>
